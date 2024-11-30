93557614ce3fd74d3ca385b1515666823c686076
Maxence <mottard.maxence@gmail.com>
2024-11-30 07:54:51 +0100

    Autoreview

--- Screens/Sources/Screens/MediaDetails/Movie/MediaDetailsMovieView.swift
+++ Screens/Sources/Screens/MediaDetails/Movie/MediaDetailsMovieView.swift
@@ -12,7 +12,7 @@ struct MediaDetailsMovieView: View {
     @State var viewModel: any MediaDetailsMovieViewModel
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        EmptyView()
     }
 }
 
